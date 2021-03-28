85beb2a854e038079930a26bf7d2b7d99156e2da
Bérénice Le Glouanec <berenice.lg@protonmail.com>
2021-03-29 01:37:12 +0200

    part2 updated

--- eval.py
+++ eval.py
@@ -74,6 +74,6 @@ if __name__ == "__main__":
         if predicted[j] == truth[j]:
             tp_tn += 1
     
-    accuracy = tp_tn / len(truth)
+    accuracy = (tp_tn / len(truth)) * 100
     print("Accuracy of the model: ", accuracy)
     
