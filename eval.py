05e1419e7472fa4eaf1aa86d07e18446b517920c
Bérénice Le Glouanec <berenice.lg@protonmail.com>
2021-03-29 00:29:21 +0200

    part2 updated

--- eval.py
+++ eval.py
@@ -4,12 +4,30 @@ import pandas as pd
 import torch
 import train
 from train import a as pre_process
-from train import g
-from train import b as instances
+
+def alter_g(x, p, correct_len):
+    z = np.zeros(correct_len)
+    z[p.index(x)] = 1
+    return z
+
+def alter_b(u, p, correct_len):
+    gt = []
+    gr = []
+    for v in range(len(u) - 4):
+        if u[v+2] not in vowels:
+            continue
+        
+        h2 = vowels.index(u[v+2])
+        gt.append(h2)
+        r = np.concatenate([alter_g(x, pn correct_len) for x in [u[v], u[v+1], u[v+3], u[v+4]]])
+        gr.append(r)
+
+    return np.array(gr), np.array(gt)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("train_model", type=str, help="A model produced by train.py.") 
+    parser.add_argument("train_data", type=str, help="A train data txt file.")
     parser.add_argument("test_data", type=str, help="A test data txt file.")
     parser.add_argument("output_filename", type=str, help="Path and filename for the output data.")
     
@@ -25,11 +43,13 @@ if __name__ == "__main__":
     processed_test_data = pre_process(args.test_data)
     
     #Create evaluation instances compatible with the training instances.
-    test_features, test_classes = instances(processed_test_data[0], processed_test_data[1])
+    processed_train_data = pre_process(args.train_data)
+    correct_len = len(processed_train_data[1])
+    test_features, test_classes = alter_b(processed_test_data[0], processed_test_data[1])
     
     #Use the model to predict instances.
-    tf = torch.Tensor(test_features)
-    tc = torch.LongTensor(test_classes)
+    tf = torch.from_numpy(test_features)
+    tc = torch.from_numpy(test_classes)
     
     pred_instances = train_model(tf.float())
     predicted = pred_instances.argmax(dim=1).numpy()
